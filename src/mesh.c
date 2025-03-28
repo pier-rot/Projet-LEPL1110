c8bca1267abd44a005b7c36fdf3c03a6c173fe04
Pierre V <6772696d@gmail.com>
2025-03-28 17:37:50 +0100

    add base file structure and txt files

--- src/mesh.c
+++ src/mesh.c
@@ -1,63 +1,63 @@
 #include "fem.h"
-// Fait par Marie Deprez & Pierre Vandermeersch
+#include <math.h>
 
+double geoSize(double x, double y)
+{
+    return 0.01;
+}
 
-
-// double geoSize(double x, double y){
-// 
-    // femGeo* theGeometry = geoGetGeometry();
-    // 
-    // return 0.01;
-// }
-
-void geoMeshGenerate() {
- 
+void geoMeshGenerate()
+{
     int ierr;
 
     double c = 0.15;
     double h = 0.1;
     double f = 0.025;
     double l = 0.7;
+    double L = 0.005;
+    double pi = 3.14159265358979323846;
+    double xc = 0.1203;
+
     double c1 = (c*c + l*l - (l*h) + ((h*h)/4.0))/(2.0*c);
     double c2 = (c*c + l*l - (l*h) - f*f + ((h*h)/4.0))/(2.0*(c-f));
+    double beta = pi - acos((c2-xc)/(c2-f));
+    double alpha = L/(c2-f);
 
-    // Extremites de l'arc
-    int A = gmshModelOccAddPoint(c,l,0,0.0,-1,&ierr); ErrorGmsh(ierr);
-    int B = gmshModelOccAddPoint(c,-l,0,0.0,-1,&ierr); ErrorGmsh(ierr);
 
-    // Coins de la poignée rectangulaire
-    int P1 = gmshModelOccAddPoint(0,h/2,0,0.0,-1,&ierr); ErrorGmsh(ierr);
-    int P2 = gmshModelOccAddPoint(f,h/2,0,0.0,-1,&ierr); ErrorGmsh(ierr);
-    int P3 = gmshModelOccAddPoint(0,-h/2,0,0.0,-1,&ierr); ErrorGmsh(ierr);
-    int P4 = gmshModelOccAddPoint(f,-h/2,0,0.0,-1,&ierr); ErrorGmsh(ierr);
+    int A = gmshModelOccAddPoint(0,0,0,0.0,-1,&ierr); 
+    int B = gmshModelOccAddPoint(0,(h/2),0,0.0,-1,&ierr); 
+    int C = gmshModelOccAddPoint(c,l,0,0.0,-1,&ierr); 
+    int D = gmshModelOccAddPoint(c2+(c2-f)*cos(beta-(alpha/2)),(h/2)+(c2-f)*sin(beta-(alpha/2)),0,0.0,-1,&ierr); 
+    int E = gmshModelOccAddPoint(c2+(c2-f)*cos(beta+(alpha/2)),(h/2)+(c2-f)*sin(beta+(alpha/2)),0,0.0,-1,&ierr); 
+    int F = gmshModelOccAddPoint(f,(h/2),0,0.0,-1,&ierr); 
+    int G = gmshModelOccAddPoint(f,0,0,0.0,-1,&ierr); 
 
-    // Centres des arcs de cercle pour contruire l'arc
-    int C1 = gmshModelOccAddPoint(c1,h/2,0,0.0,-1,&ierr); ErrorGmsh(ierr);
-    int C2 = gmshModelOccAddPoint(c2,h/2,0,0.0,-1,&ierr); ErrorGmsh(ierr);
-    int C3 = gmshModelOccAddPoint(c1,-h/2,0,0.0,-1,&ierr); ErrorGmsh(ierr);
-    int C4 = gmshModelOccAddPoint(c2,-h/2,0,0.0,-1,&ierr); ErrorGmsh(ierr);
-    // DimTags array
+    int C1 = gmshModelOccAddPoint(c1,h/2,0,0.0,-1,&ierr); 
+    int C2 = gmshModelOccAddPoint(c2,h/2,0,0.0,-1,&ierr); 
+
+    // dimtags for center points
     int C1dt [] = {0,C1};
     int C2dt [] = {0,C2};
-    int C3dt [] = {0,C3};
-    int C4dt [] = {0,C4};
-    int* center_points [] = {C1dt,C2dt,C3dt,C4dt};
+    int* center_points [] = {C1dt,C2dt};
 
+
+    int bottom_handle = gmshModelOccAddLine(A,G,-1,&ierr); 
+    int right_handle = gmshModelOccAddLine(G,F,-1,&ierr); 
+    int right_arc = gmshModelOccAddCircleArc(F,C2,E,-1,1,&ierr); 
+    int tension_arc = gmshModelOccAddCircleArc(E,C2,D,-1,1,&ierr); 
+    int right_tip = gmshModelOccAddCircleArc(D,C2,C,-1,1,&ierr); 
+    int left_arc = gmshModelOccAddCircleArc(C,C1,B,-1,1,&ierr); 
+    int left_handle = gmshModelOccAddLine(B,A,-1,&ierr); 
     // Creation des arcs de cercle
-    int top_left_arc = gmshModelOccAddCircleArc(A,C1,P1,-1,1,&ierr); ErrorGmsh(ierr);
-    int top_right_arc = gmshModelOccAddCircleArc(P2,C2,A,-1,1,&ierr); ErrorGmsh(ierr);
-    int bottom_left_arc = gmshModelOccAddCircleArc(P3,C3,B,-1,1,&ierr); ErrorGmsh(ierr);
-    int bottom_right_arc = gmshModelOccAddCircleArc(B,C4,P4,-1,1,&ierr); ErrorGmsh(ierr);
-    int left_handle = gmshModelOccAddLine(P1,P3,-1,&ierr); ErrorGmsh(ierr);
-    int right_handle = gmshModelOccAddLine(P4,P2,-1,&ierr); ErrorGmsh(ierr);
-    int curves [] = {top_left_arc,left_handle,bottom_left_arc,bottom_right_arc,right_handle,top_right_arc};
-    int contour = gmshModelOccAddCurveLoop(curves,6,-1,&ierr); ErrorGmsh(ierr);
+    int curves [] = {bottom_handle,right_handle,right_arc,tension_arc,right_tip,left_arc,left_handle};
+    int contour = gmshModelOccAddCurveLoop(curves,7,-1,&ierr); 
     int contourArr [] = {contour};
-    int plane = gmshModelOccAddPlaneSurface(contourArr,1,-1,&ierr); ErrorGmsh(ierr);
-    for(int i = 0; i < 4; i++){
-        gmshModelOccRemove(center_points[i],2,0,&ierr); ErrorGmsh(ierr);
-    }
-    
+    int plane = gmshModelOccAddPlaneSurface(contourArr,1,-1,&ierr); 
+    for(int i = 0; i < 2; i++){
+        gmshModelOccRemove(center_points[i],2,0,&ierr); 
+    }              
+   
+    geoSetSizeCallback(geoSize);
                                   
     gmshModelOccSynchronize(&ierr);
     gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
@@ -71,6 +71,5 @@ void geoMeshGenerate() {
 //    gmshOptionSetNumber("Mesh.Algorithm", 8, &ierr);
 //    gmshOptionSetNumber("Mesh.RecombinationAlgorithm", 1.0, &ierr);
 //    gmshModelGeoMeshSetRecombine(2,1,45,&ierr);
-//    gmshModelMeshGenerate(2, &ierr);  
-   
+//    gmshModelMeshGenerate(2, &ierr);
 }
