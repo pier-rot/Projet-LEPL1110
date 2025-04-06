3d72b091906c6198f075bfdc60f86ebb4b8a0aad
marie <m.deprez@student.uclouvain.be>
2025-04-06 16:21:59 +0200

    fonction geosize

---
+++ src/geomesh.c
@@ -0,0 +1,22 @@
+#include "utils.h"
+#include <math.h>
+
+/*
+This function is used to get the geometry size of the plate at a given point.
+
+@params:
+    - x: the x coordinate of the point at which we want to get the geometry size.
+    - y: the y coordinate of the point at which we want to get the geometry size.
+
+@returns:
+    - the geometry size of the plate at the point (x, y).
+*/
+
+double geoSize(double x, double y)
+{   
+
+    //TO DO
+    double size = 0.0;
+    
+    return 1.0;
+}
